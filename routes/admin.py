c4fb1f9d9344ad13bdbaa30c4b5f062b92ee47d9
Sebastian Tapia <sebastian.tapia9519@gmail.com>
2026-02-12 14:29:14 -0600

    Implementando R2

--- routes/admin.py
+++ routes/admin.py
@@ -1,4 +1,6 @@
+import os
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash
 from datetime import datetime, timedelta, timezone
 from db import get_db_connection
