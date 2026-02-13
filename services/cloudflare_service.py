8a45357ce28d0ba331290cee7e9295d5ea0a5503
Sebastian Tapia <sebastian.tapia9519@gmail.com>
2026-02-12 20:44:46 -0600

    Creando Invitaciones

--- services/cloudflare_service.py
+++ services/cloudflare_service.py
@@ -2,12 +2,15 @@ import boto3 # Librería estándar para R2/S3
 from flask import current_app
 
 def upload_to_cloudflare(file, folder="invitaciones"):
-    # Aquí configurarás tus credenciales de Cloudflare R2
     s3 = boto3.client('s3',
-        endpoint_url=f"https://e063cc1ad223c0544aee7a03d9f0f9a6.r2.cloudflarestorage.com",
-        aws_access_key_id=current_app.config['5dad301112cb3db90de60278e5d4e101'],
-        aws_secret_access_key=current_app.config['8d6b5dc8d9b01a8196b9e1a7d3e425f600cefad5e189bf42f0264edde035ab70'],
-        region_name = 'auto'
+        # Pega tu URL completa aquí directamente entre comillas
+        endpoint_url="https://5dad301112cb3db90de60278e5d4e101.r2.cloudflarestorage.com",
+        
+        # Pega tus claves directamente aquí (luego las pasamos a variables de entorno para seguridad)
+        aws_access_key_id="8d6b5dc8d9b01a8196b9e1a7d3e425f600cefad5e189bf42f026...", 
+        aws_secret_access_key="TU_SECRET_KEY_AQUI_LA_QUE_EMPIEZA_CON_NUMEROS_O_LETRAS",
+        
+        region_name='auto'
     )
     
     filename = f"{folder}/{file.filename}"
