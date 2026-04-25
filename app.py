23fe1956bc794b9db9c4fab739633f68a83bdd1c
Sebastian Tapia <sebastian.tapia9519@gmail.com>
2026-04-24 18:10:20 -0600

    Add gunicorn to dependencies

--- app.py
+++ app.py
@@ -89,8 +89,9 @@ log.setLevel(logging.ERROR)
 # Configuración de Mail desde variables de entorno
 app.config.update(
     MAIL_SERVER=os.environ.get('MAIL_SERVER'),
-    MAIL_PORT=int(os.environ.get('MAIL_PORT', 465)),
-    MAIL_USE_SSL=os.environ.get('MAIL_USE_SSL', 'True') == 'True',
+    MAIL_PORT=int(os.environ.get('MAIL_PORT', 587)),
+    MAIL_USE_SSL=os.environ.get('MAIL_USE_SSL', 'False').lower() == 'true',
+    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS', 'False').lower() == 'true',
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD'),
     MAIL_DEFAULT_SENDER=os.environ.get('MAIL_USERNAME')
