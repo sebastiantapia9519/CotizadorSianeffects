3e17cb2e99bb3b08a16d46d7bb116124c19f52a8
Sebastian Tapia <sebastian.tapia9519@gmail.com>
2026-04-24 18:20:40 -0600

    Add gunicorn to dependencies

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ app.config.update(
     MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS', 'False').lower() == 'true',
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD'),
-    MAIL_DEFAULT_SENDER=os.environ.get('MAIL_USERNAME')
+    MAIL_DEFAULT_SENDER=os.environ.get('contacto@sianeffects.com')
 )
 
 # Inicializamos la extensión con la app configurada
